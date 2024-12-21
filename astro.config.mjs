47d5c8c11781eec6afacc9ded72ff5f361050dc6
CodeF1ow <k1ri86mail@gmail.com>
2024-12-21 17:41:22 +0100

    Fixed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: netlify(),
+  integrations: [tailwind(), netlify()], // Asegúrate de incluir `netlify` como integración
+  output: 'server', // Cambia 'hybrid' a 'server' o 'static' según lo que necesites
 });
+
